a0d5555ba303c5eb253c8c2ae65a761d1b2b2c79
s23087 <s23087@pjwstk.edu.pl>
2025-03-14 23:01:24 +0100

    added test git

---
+++ ConsoleFunctionTests/ConsoleFunctionTests.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleOrderExecutor.ConsoleFunction;
+using ConsoleOrderExecutor.ConsoleFunction.Utils;
+using ConsoleOrderExecutor.Orders.DTOs;
+using ConsoleOrderExecutor.Orders.Services;
+using ConsoleOrderExecutor.Products.Services;
+using FakeItEasy;
+
+namespace ConsoleOrderExecutor.Tests.ConsoleFunctionTests
+{
+    public class ConsoleFunctionTests
+    {
+        private readonly IConsoleUtils _consoleUtils;
+        private readonly IOrderService _orderService;
+        private readonly IProductService _productService;
+
+        public ConsoleFunctionTests()
+        {
+            _consoleUtils = A.Fake<IConsoleUtils>();
+            _orderService = A.Fake<IOrderService>();
+            _productService = A.Fake<IProductService>();
+        }
+        [Fact]
+        public void ConsoleFunctions_CreateNewOrder_OrderCreated()
+        {
+            //Arrange
+            ConsoleFunctions consoleFunctions = new(_consoleUtils, _orderService, _productService);
+            var strWriter = new StringWriter();
+            Console.SetOut(strWriter);
+            string? companyOut = "1";
+            A.CallTo(() => _consoleUtils.GetParameter("Is this order for company or physical person? Type 1 if yes or 0 if false.", A<Predicate<string?>>.Ignored, out companyOut)).Returns(false);
+            string? addressOut = "address";
+            A.CallTo(() => _consoleUtils.GetParameter("Please write delivery address (max 250 characters).", A<Predicate<string?>>.Ignored, out addressOut)).Returns(false);
+            var paymentList = new List<GetPaymentOption>() {
+                new() {
+                    Id = 2,
+                    Name = ""
+                }
+            };
+            var paymentInfoInString = paymentList.Select(x => x.Id + " - " + x.Name);
+            A.CallTo(() => _orderService.GetPaymentOptions()).Returns(paymentList);
+            string? paymentOut = "2";
+            A.CallTo(() => _consoleUtils.GetParameter("Please choose the payment option, by writing its number.\\n" + String.Join("\\n", paymentInfoInString), A<Predicate<string?>>.Ignored, out paymentOut)).Returns(false);
+            string? eanOut = "2342343";
+            A.CallTo(() => _consoleUtils.GetParameter("Please write ean of product you want to add.", A<Predicate<string?>>.Ignored, out eanOut)).Returns(false);
+            A.CallTo(() => _productService.ProductExist(A<string>.Ignored)).Returns(true);
+            string? priceOut = "20.5";
+            A.CallTo(() => _consoleUtils.GetParameter("Please write product price.", A<Predicate<string?>>.Ignored, out priceOut)).Returns(false);
+            string? nextStepOut = "done";
+            A.CallTo(() => _consoleUtils.GetParameter("If you done write done, otherwise next.", A<Predicate<string?>>.Ignored, out nextStepOut)).Returns(false);
+            A.CallTo(() => _orderService.CreateOrder(A<CreateOrder>.Ignored)).Returns(true);
+
+            //Act
+            consoleFunctions.CreateNewOrder();
+
+            //Assert
+            Assert.DoesNotContain("Error:", strWriter.ToString());
+            Assert.Contains("Order created.", strWriter.ToString());
+        }
+    }
+}
---
+++ ConsoleFunctionTests/ConsoleFunctionUtilsTests/ConsoleUtilsTests.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleOrderExecutor.ConsoleFunction.Utils;
+
+namespace ConsoleOrderExecutor.Tests.ConsoleFunctionTests.ConsoleFunctionUtilsTests
+{
+    public class ConsoleUtilsTests
+    {
+
+        [Fact]
+        public void ConsoleUtils_GetParameter_ValuePassed_ReturnFalse()
+        {
+            //Arrange
+            ConsoleUtils consoleUtils = new();
+            string text = "text";
+            static bool predicate(string? a) => a != null;
+            var strReader = new StringReader("test");
+            Console.SetIn(strReader);
+            string outValue = "test";
+
+            //Act
+            var result = consoleUtils.GetParameter(text, predicate, out string? value);
+
+            //Assert
+            Assert.False(result);
+            Assert.Equal(outValue, value);
+        }
+
+        [Fact]
+        public void ConsoleUtils_GetParameter_ExitPassed_ReturnTrue()
+        {
+            //Arrange
+            ConsoleUtils consoleUtils = new();
+            string text = "text";
+            static bool predicate(string? a) => a != null;
+            var strReader = new StringReader("exit");
+            Console.SetIn(strReader);
+
+            //Act
+            var result = consoleUtils.GetParameter(text, predicate, out string? value);
+
+            //Assert
+            Assert.Null(value);
+            Assert.True(result);
+        }
+        [Fact]
+        public void ConsoleUtils_GetParameter_InvalidFirstValue_ReturnFalse()
+        {
+            //Arrange
+            ConsoleUtils consoleUtils = new();
+            string text = "text";
+            static bool predicate(string? a) => a != "text";
+            var input = String.Join(Environment.NewLine, new[]
+            {
+                "text", "test"
+            });
+            var strReader = new StringReader(input);
+            Console.SetIn(strReader);
+            var output = String.Join(Environment.NewLine, new[]
+           {
+                text, "Invalid value.", ""
+            });
+            var strWriter = new StringWriter();
+            Console.SetOut(strWriter);
+            string outValue = "test";
+
+            //Act
+            var result = consoleUtils.GetParameter(text, predicate, out string? value);
+
+            //Assert
+            Assert.Equal(output, strWriter.ToString());
+            Assert.Equal(outValue, value);
+            Assert.False(result);
+        }
+
+    }
+}
